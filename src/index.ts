983918e87f12e3d36889adb28833df9f6db52357
Joely Brito <joely_brito@outlook.com>
2022-09-10 17:36:08 -0300

    Adicionado endpoint que cria um novo docente

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import updateDocenteTurma from "./data/updateDocenteTurma";
 import updateMudaEstudanteTurma from "./data/updateMudaEstudanteTurma";
 import app from "./app";
 import { Turma } from "./classes/Turma";
+import { Docente } from "./classes/Docente"
+import createDocente from "./data/createDocente"
 import createTurma from "./data/createTurma";
 
 // ********** C Read UD **********
@@ -51,6 +53,27 @@ app.get("/estudante/:nome_estudante", async (req: Request, res: Response) => {
     res.send(estudente);
   } catch (error) {
     console.log(error);
+  }
+});
+
+// Criar um novo docente
+
+app.post("/docente", async (req: Request, res: Response) => {
+  try {
+    const id_docente = req.body.id_docente;
+    const nome_docente = req.body.nome_docente;
+    const mail_docente = req.body.email_docente;
+    const data_nasc_docente = req.body.data_nasc_docente
+    const turma_id = req.body.turma_id
+    const novoDocente = new Docente(id_docente, nome_docente, mail_docente, data_nasc_docente, turma_id);
+    await createDocente(novoDocente);
+    res.status(201).send({
+      message: "Novo docente criado!!",
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Error ao criar docente!!",
+    });
   }
 });
 
