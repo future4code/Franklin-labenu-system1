c4f5335ce6ee3b1df62ed18b3b10d996904f0651
Joely Brito <joely_brito@outlook.com>
2022-09-13 11:09:32 -0300

    Adicionado idGereration a criação de nova docente

--- src/endpoints/criaNovoDocente.ts
+++ src/endpoints/criaNovoDocente.ts
@@ -4,13 +4,11 @@ import createDocente from "../data/createDocente";
 
 export const criaNovoDocente = async (req: Request, res: Response) => {
   try {
-    const id_docente = req.body.id_docente;
     const nome_docente = req.body.nome_docente;
     const mail_docente = req.body.email_docente;
     const data_nasc_docente = req.body.data_nasc_docente;
     const turma_id = req.body.turma_id;
     const novoDocente = new Docente(
-      id_docente,
       nome_docente,
       mail_docente,
       data_nasc_docente,
