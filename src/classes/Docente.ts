001c30c48cdd8610f900137c16155832dc13b2cb
Joely Brito <joely_brito@outlook.com>
2022-09-13 11:08:19 -0300

    Adicionado idGereration a classe docente

--- src/classes/Docente.ts
+++ src/classes/Docente.ts
@@ -1,3 +1,5 @@
+import { IdGenerator } from "../services/IdGenerator";
+
 export class Docente {
   id_docente: string;
   nome_docente: string;
@@ -6,16 +8,15 @@ export class Docente {
   turma_id: string;
 
   constructor(
-    id_docente: string,
     nome_docente: string,
     email_docente: string,
     data_nasc_docente: string,
     turma_id: string
   ) {
-    this.id_docente = id_docente;
+    this.id_docente = new IdGenerator().generateId()
     this.nome_docente = nome_docente;
     this.email_docente = email_docente;
     this.data_nasc_docente = data_nasc_docente;
     this.turma_id = turma_id;
   }
-}
+};
