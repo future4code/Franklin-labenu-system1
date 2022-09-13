ea43ac6afbceaaec93530e627fe1669c8fe7a303
Joely Brito <joely_brito@outlook.com>
2022-09-13 11:09:50 -0300

    Adicionado idGereration a criação de novo estudante

--- src/endpoints/criaNovoEstudante.ts
+++ src/endpoints/criaNovoEstudante.ts
@@ -4,13 +4,11 @@ import createEstudante from "../data/createEstudante";
 
 export const criaNovoEstudante = async (req: Request, res: Response) => {
   try {
-    const id_estudante = req.body.id_estudante;
     const nome_estudante = req.body.nome_estudante;
     const email_estudante = req.body.email_estudante;
     const data_nasc_estudante = req.body.data_nasc_estudante;
     const turma_id = req.body.turma_id;
     const novoEstudante = new Estudante(
-      id_estudante,
       nome_estudante,
       email_estudante,
       data_nasc_estudante,
