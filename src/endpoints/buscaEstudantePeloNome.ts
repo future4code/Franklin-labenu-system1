f040f0d04658db025e20c77291e5fc30ca6c194e
Joely Brito <joely_brito@outlook.com>
2022-09-13 12:43:47 -0300

    Realizado debug

--- src/endpoints/buscaEstudantePeloNome.ts
+++ src/endpoints/buscaEstudantePeloNome.ts
@@ -4,6 +4,7 @@ import getStudentByName from "../data/getStudentByName";
 export const buscaEstudantePeloNome = async (req: Request, res: Response) => {
   try {
     const nome_estudante = req.params.nome_estudante;
+    console.log(nome_estudante)
     const estudante = await getStudentByName(nome_estudante);
     res.send(estudante);
   } catch (error) {
